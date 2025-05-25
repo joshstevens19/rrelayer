38f75432b9f4542c610f762e63a81133681d3090
Josh Stevens <joshstevens19@hotmail.co.uk>
2025-05-25 23:31:44 +0100

    refactor: gas API

--- crates/core/src/gas/api/mod.rs
+++ crates/core/src/gas/api/mod.rs
@@ -18,12 +18,15 @@ async fn get_gas_price(
     State(state): State<Arc<AppState>>,
     Path(chain_id): Path<ChainId>,
 ) -> Result<Json<GasEstimatorResult>, StatusCode> {
-    let gas_oracle = state.gas_oracle_cache.lock().await.get_gas_price(&chain_id).await;
+    let gas_oracle = state
+        .gas_oracle_cache
+        .lock()
+        .await
+        .get_gas_price(&chain_id)
+        .await
+        .ok_or(StatusCode::NOT_FOUND)?;
 
-    match gas_oracle {
-        Some(gas_oracle) => Ok(Json(gas_oracle)),
-        None => Err(StatusCode::NOT_FOUND),
-    }
+    Ok(Json(gas_oracle))
 }
 
 pub fn create_gas_routes() -> Router<Arc<AppState>> {
