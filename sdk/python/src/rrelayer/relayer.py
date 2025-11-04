4a0afb9da30b52da1dbf338a6838249bb71c7c9a
parizval <anmolgoyal@gmail.com>
2025-11-04 17:08:18 +0530

    feat(python-sdk): added basic functions and structure to RelayerClient

--- sdk/python/src/rrelayer/relayer.py
+++ sdk/python/src/rrelayer/relayer.py
@@ -1,12 +1,53 @@
 from pydantic import BaseModel, ConfigDict, PrivateAttr
-from web3 import AsyncWeb3
+from web3 import AsyncWeb3, Web3
+from typing import Any
+from rrelayer.api import API
 
 
-class Relayer(BaseModel):
+class RelayerClient(BaseModel):
+    class AllowList:
+        def __init__(self, relayerClient: "RelayerClient"):
+            self._relayer: "RelayerClient" = relayerClient
+
+        async def get(self):
+            pass
+
+    class Sign:
+        def __init__(self, relayerClient: "RelayerClient"):
+            self._relayer: "RelayerClient" = relayerClient
+
+    class Transaction:
+        def __init__(self, relayerClient: "RelayerClient"):
+            self._relayer: "RelayerClient" = relayerClient
+
+        async def get(self):
+            pass
+
+        async def getStatus(self):
+            pass
+
+        async def getAll(self):
+            pass
+
+        async def replace(self):
+            pass
+
+        async def cancel(self):
+            pass
+
+        async def send(self):
+            pass
+
     _id: str = PrivateAttr()
 
     _apiBaseConfig: dict[str, str] = PrivateAttr()
     _ethereumProvider: AsyncWeb3 = PrivateAttr()
+
+    _api: API = PrivateAttr()
+
+    _allowlist: AllowList = PrivateAttr()
+    _sign: Sign = PrivateAttr()
+    _transaction: Transaction = PrivateAttr()
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -35,6 +76,45 @@ class Relayer(BaseModel):
         else:
             raise ValueError("Invalid authentication credentials")
 
+        self._api = API()
+
+        self._allowlist = self.AllowList(self)
+        self._sign = self.Sign(self)
+        self._transaction = self.Transaction(self)
+
     @property
     def name(self):
         return self._id
+
+    @property
+    def allowlist(self) -> AllowList:
+        return self._allowlist
+
+    @property
+    def sign(self) -> Sign:
+        return self._sign
+
+    @property
+    def transaction(self) -> Transaction:
+        return self._transaction
+
+    async def address(self) -> str | None:
+        response = await self.getInfo()
+        return response["address"] if response else None
+
+    async def getInfo(self):
+        response = await self._api.getApi(self._apiBaseConfig, f"relayers/{self._id}")
+        return response["relayer"] if response else None
+
+    async def getBalanceOf(self):
+        address = await self.address()
+        if address:
+            balance = await self._ethereumProvider.eth.get_balance(
+                Web3.to_checksum_address(address)
+            )
+            return Web3.from_wei(balance, "ether")
+        else:
+            return 0
+
+    def ethereumProvider(self) -> AsyncWeb3[Any]:
+        return self._ethereumProvider
