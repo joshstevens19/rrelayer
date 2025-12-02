7498227e2e6c9066181e8a86e958142ed1cbff4d
Josh Stevens <joshstevens19@hotmail.co.uk>
2025-12-02 13:52:28 +0000

    fix: bad migration when already ran

--- crates/core/src/schema/v1_0_1.rs
+++ crates/core/src/schema/v1_0_1.rs
@@ -4,7 +4,7 @@ use crate::postgres::{PostgresClient, PostgresError};
 pub async fn apply_v1_0_1_schema(client: &PostgresClient) -> Result<(), PostgresError> {
     let schema_sql = r#"
         ALTER TABLE relayer.record
-        ADD COLUMN cloned_from_chain_id BIGINT DEFAULT NULL;
+        ADD COLUMN IF NOT EXISTS cloned_from_chain_id BIGINT DEFAULT NULL;
     "#;
 
     client.batch_execute(schema_sql).await?;
